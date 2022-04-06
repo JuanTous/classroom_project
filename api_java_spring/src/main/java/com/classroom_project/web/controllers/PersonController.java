5d546d3d6302bad5da5d8a52d58f740dae16c549
Juan Tous <tousjuan01@gmail.com>
2022-04-06 18:44:43 -0500

    React app initialized and added Cors for the API

--- api_java_spring/src/main/java/com/classroom_project/web/controllers/PersonController.java
+++ api_java_spring/src/main/java/com/classroom_project/web/controllers/PersonController.java
@@ -8,6 +8,7 @@ import com.classroom_project.persistence.entities.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/people")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PersonController {
     @Autowired
     private PersonService service;
